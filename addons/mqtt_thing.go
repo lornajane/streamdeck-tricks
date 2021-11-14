55a8ae788d28baaa02fcb486a75be9957cba2e0c
Lorna Jane Mitchell <lorna@lornajane.net>
2021-11-14 21:00:06 +0000

    Update MQTT add-on for new LED wall

--- addons/mqtt_thing.go
+++ addons/mqtt_thing.go
@@ -26,99 +26,100 @@ func (p *MqttThing) Init() {
 	}
 }
 
+type LEDWallBg struct {
+	Action string `json:"action"`
+	Red   uint8 `json:"r"`
+	Green uint8 `json:"g"`
+	Blue  uint8 `json:"b"`
+}
+
+type LEDWallFirework struct {
+	Action string `json:"action"`
+	Hue uint8 `json:"b,omitempty"`
+}
+type LEDWallSnake struct {
+	Action string `json:"action"`
+	Enabled bool `json:"enabled"`
+}
+
 // Set up buttons
-type LEDColour struct {
-	Red   uint8 `mapstructure:"red" json:"red"`
-	Green uint8 `mapstructure:"green" json:"green"`
-	Blue  uint8 `mapstructure:"blue" json:"blue"`
-	White uint8 `mapstructure:"white" json:"white"`
-}
-
-type PlugDevice struct {
-	Name     string `mapstructure:"name"`
-	ButtonId int    `mapstructure:"button"`
-	ImageOn  string `mapstructure:"image_on"`
-	ImageOff string `mapstructure:"image_off"`
-}
-
-type ManyLights struct {
-	Colour1 LEDColour `mapstructure:"colour1"`
-	Colour2 LEDColour `mapstructure:"colour2"`
-}
-
 func (p *MqttThing) Buttons() {
-	var lights []ManyLights
-	viper.UnmarshalKey("shelf_lights", &lights)
 	button_index := 18
 
-	for _, light := range lights {
-		colour := color.RGBA{light.Colour1.Red, light.Colour1.Green, light.Colour1.Blue, 255}
-		lbutton := buttons.NewColourButton(colour)
-		lbutton.SetActionHandler(&MQTTAction{Colour: light.Colour1, Colour2: light.Colour2, Client: p.mqtt_client})
-		p.SD.AddButton(button_index, lbutton)
-		button_index = button_index + 1
-	}
+	bgoptions := LEDWallBg{"background", 5, 0, 6}
+	lbutton := buttons.NewColourButton(color.RGBA{255,0,255,255})
+	lbutton.SetActionHandler(&MQTTBgAction{Options: bgoptions, Client: p.mqtt_client})
+	p.SD.AddButton(button_index, lbutton)
+	button_index = button_index + 1
 
-	// on/off plugs
-	var buttons_plug map[string]PlugDevice // MQTT-enabled on/off plugs
-	viper.UnmarshalKey("plug_devices", &buttons_plug)
-	for device, deets := range buttons_plug {
-		// assume off, we can't get state
-		image := viper.GetString("buttons.images") + "/" + deets.ImageOff
-		plugbutton, err := buttons.NewImageFileButton(image)
-		if err == nil {
-			plugaction := &PlugAction{Client: p.mqtt_client, Device: device, State: 0, ImageOn: deets.ImageOn, ImageOff: deets.ImageOff}
-			plugbutton.SetActionHandler(plugaction)
-			p.SD.AddButton(deets.ButtonId, plugbutton)
-		} else {
-			log.Warn().Err(err)
-		}
+	bgoptions2 := LEDWallBg{"background", 4, 3, 0}
+	lbutton2 := buttons.NewColourButton(color.RGBA{255,255,155,255})
+	lbutton2.SetActionHandler(&MQTTBgAction{Options: bgoptions2, Client: p.mqtt_client})
+	p.SD.AddButton(button_index, lbutton2)
+
+	fireworkoptions := LEDWallFirework{"firework", 0}
+	fbutton, ferr := buttons.NewImageFileButton(viper.GetString("buttons.images") + "/firework-sparkler.png")
+	if(ferr != nil) {
+		panic(ferr)
 	}
+	fbutton.SetActionHandler(&MQTTFireworkAction{Options: fireworkoptions, Client: p.mqtt_client})
+	p.SD.AddButton(16, fbutton)
+
+	snakeoptions := LEDWallSnake{"snake", false}
+	sbutton := buttons.NewTextButton("Snake")
+	sbutton.SetActionHandler(&MQTTSnakeAction{Options: snakeoptions, Client: p.mqtt_client})
+	p.SD.AddButton(17, sbutton)
+
 }
 
-// Button action handler
-type PlugAction struct {
-	Client   mqtt.Client
-	Device   string
-	State    int
-	ImageOn  string
-	ImageOff string
-}
-
-func (action *PlugAction) Pressed(btn streamdeck.Button) {
-	imagebutton := btn.(*buttons.ImageFileButton)
-
-	// Toggle! Are we on? Turn off! Not on? Turn on!
-	if action.State == 1 {
-		// on! Turn off
-		token := action.Client.Publish("/house/plug/"+action.Device, 0, false, "0")
-		token.Wait()
-		imagebutton.SetFilePath(viper.GetString("buttons.images") + "/" + action.ImageOff)
-		action.State = 0
-	} else {
-		// off! So turn on
-		token := action.Client.Publish("/house/plug/"+action.Device, 0, false, "1")
-		token.Wait()
-		imagebutton.SetFilePath(viper.GetString("buttons.images") + "/" + action.ImageOn)
-		action.State = 1
-	}
-}
-
-// Lights Action handler
-type MQTTAction struct {
+type MQTTBgAction struct {
 	Client  mqtt.Client
-	Colour  LEDColour
-	Colour2 LEDColour
+	Options LEDWallBg
 	btn     streamdeck.Button
 }
 
-func (action *MQTTAction) Pressed(btn streamdeck.Button) {
-	var targetColour []LEDColour
-	targetColour = append(targetColour, action.Colour)
-	targetColour = append(targetColour, action.Colour2)
 
-	payload, _ := json.Marshal(targetColour)
+func (action *MQTTBgAction) Pressed(btn streamdeck.Button) {
+	payload, _ := json.Marshal(action.Options)
 	log.Debug().Msg(string(payload))
-	token := action.Client.Publish("/shelf/lights", 0, false, payload)
+	token := action.Client.Publish("/ledwall/1/request", 0, false, payload)
 	token.Wait()
+}
+
+type MQTTFireworkAction struct {
+	Client  mqtt.Client
+	Options LEDWallFirework
+	btn     streamdeck.Button
+}
+
+
+func (action *MQTTFireworkAction) Pressed(btn streamdeck.Button) {
+	payload, _ := json.Marshal(action.Options)
+	log.Debug().Msg(string(payload))
+	token := action.Client.Publish("/ledwall/1/request", 0, false, payload)
+	token.Wait()
+}
+
+type MQTTSnakeAction struct {
+	Client      mqtt.Client
+	Options     LEDWallSnake
+	btn         streamdeck.Button
+	snake_state bool
+}
+
+
+func (action *MQTTSnakeAction) Pressed(btn streamdeck.Button) {
+	// use current snake state before sending, then switch state after
+	action.Options.Enabled = action.snake_state
+	payload, _ := json.Marshal(action.Options)
+	log.Debug().Msg(string(payload))
+	token := action.Client.Publish("/ledwall/1/request", 0, false, payload)
+	token.Wait()
+
+	// now toggle state ready for next press
+	if(action.snake_state) {
+		action.snake_state = false
+	} else {
+		action.snake_state = true
+	}
 }
